240c28512fb85f5b14eb28a9a2ec3098ddd0b166
jms1999lsc <joaomsilva7@gmail.com>
2025-10-16 13:47:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ st.markdown(
     /* Sidebar mais estreita e com fundo leve */
     [data-testid="stSidebar"] {{
         min-width: 220px !important;
-        max-width: 220px !important;
+        max-width: 400px !important;
         background-color: #f3f3f3;
         padding-top: 1.0rem;
         padding-left: 0.6rem;
